bebcbbbf8aca52d879141915d4b88d612fde260f
Gimenz <agaaja187@gmail.com>
2024-03-18 03:42:46 +0700

    initial commit

---
+++ helpers/values.js
@@ -0,0 +1,149 @@
+let urlProvinsiBMKG = [
+    {
+        provinsi: 'aceh',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-Aceh.xml',
+    },
+    {
+        provinsi: 'bali',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-Bali.xml',
+    },
+    {
+        provinsi: 'bangkaBelitung',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-BangkaBelitung.xml',
+    },
+    {
+        provinsi: 'banten',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-Banten.xml',
+    },
+    {
+        provinsi: 'bengkulu',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-Bengkulu.xml',
+    },
+    {
+        provinsi: 'diYogyakarta',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-DIYogyakarta.xml',
+    },
+    {
+        provinsi: 'dkiJakarta',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-DKIJakarta.xml',
+    },
+    {
+        provinsi: 'gorontalo',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-Gorontalo.xml',
+    },
+    {
+        provinsi: 'jambi',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-Jambi.xml',
+    },
+    {
+        provinsi: 'jawaBarat',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-JawaBarat.xml',
+    },
+    {
+        provinsi: 'jawaTengah',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-JawaTengah.xml',
+    },
+    {
+        provinsi: 'jawaTimur',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-JawaTimur.xml',
+    },
+    {
+        provinsi: 'kalimantanBarat',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-KalimantanBarat.xml',
+    },
+    {
+        provinsi: 'kalimantanSelatan',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-KalimantanSelatan.xml',
+    },
+    {
+        provinsi: 'kalimantanTengah',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-KalimantanTengah.xml',
+    },
+    {
+        provinsi: 'kalimantanTimur',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-KalimantanTimur.xml',
+    },
+    {
+        provinsi: 'kalimantanUtara',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-KalimantanUtara.xml',
+    },
+    {
+        provinsi: 'kepulauanRiau',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-KepulauanRiau.xml',
+    },
+    {
+        provinsi: 'lampung',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-Lampung.xml',
+    },
+    {
+        provinsi: 'maluku',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-Maluku.xml',
+    },
+    {
+        provinsi: 'malukuUtara',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-MalukuUtara.xml',
+    },
+    {
+        provinsi: 'nusaTenggaraBarat',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-NusaTenggaraBarat.xml',
+    },
+    {
+        provinsi: 'nusaTenggaraTimur',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-NusaTenggaraTimur.xml',
+    },
+    {
+        provinsi: 'papua',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-Papua.xml',
+    },
+    {
+        provinsi: 'papuaBarat',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-PapuaBarat.xml',
+    },
+    {
+        provinsi: 'riau',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-Riau.xml',
+    },
+    {
+        provinsi: 'sulawesiBarat',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-SulawesiBarat.xml',
+    },
+    {
+        provinsi: 'sulawesiSelatan',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-SulawesiSelatan.xml',
+    },
+    {
+        provinsi: 'sulawesiTengah',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-SulawesiTengah.xml',
+    },
+    {
+        provinsi: 'sulawesiTenggara',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-SulawesiTenggara.xml',
+    },
+    {
+        provinsi: 'sulawesiUtara',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-SulawesiUtara.xml',
+    },
+    {
+        provinsi: 'sumateraBarat',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-SumateraBarat.xml',
+    },
+    {
+        provinsi: 'sumateraSelatan',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-SumateraSelatan.xml',
+    },
+    {
+        provinsi: 'sumateraUtara',
+        url: 'https://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-SumateraUtara.xml',
+    },
+];
+
+const baseURL = {
+    dataWeb: 'https://dataweb.bmkg.go.id',
+    dataApp: 'https://api-apps.bmkg.go.id',
+    data: 'https://data.bmkg.go.id'
+}
+
+module.exports = {
+    urlProvinsiBMKG,
+    baseURL
+}
---
+++ index.js
@@ -0,0 +1,81 @@
+const { default: axios } = require('axios');
+const { transform } = require('camaro');
+const { urlProvinsiBMKG, baseURL } = require('./helpers/values');
+const { XMLParser } = require('fast-xml-parser');
+
+const xml_parser = new XMLParser({
+    ignoreAttributes: false,
+    attributeNamePrefix: ""
+})
+
+class BMKG {
+    constructor() { }
+
+    static infoGempa = async () => {
+        const url = {
+            gempa: baseURL.data + '/DataMKG/TEWS/autogempa.xml',
+            image: baseURL.data + '/DataMKG/TEWS/',
+        };
+        const template = [
+            'Infogempa/gempa',
+            {
+                tanggal: 'Tanggal',
+                jam: 'Jam',
+                lintang: 'Lintang',
+                bujur: 'Bujur',
+                magnitude: 'Magnitude',
+                kedalaman: 'Kedalaman',
+                potensi: 'Potensi',
+                wilayah: 'Wilayah',
+                dirasakan: 'Dirasakan',
+                shakemap: 'Shakemap',
+            },
+        ];
+
+        const { data } = await axios.get(url.gempa, { responseType: 'text' })
+        const json = await transform(data, template)
+        json[0].shakemap = url.image + json[0].shakemap;
+        return json
+    }
+
+    /**
+     * mengambil data nama kabupaten
+     * @param {string} provinsi provinsi, contoh jawatengah (tanpa spasi)
+     * @returns 
+     */
+    static Provinsi = async (provinsi) => {
+        let find = urlProvinsiBMKG.find(x => x.provinsi.toLowerCase() == provinsi.toLowerCase())
+        const { data } = await axios.get(find.url)
+
+        return xml_parser.parse(data).data
+    }
+
+    /**
+     * mengambil data nama kecamatan
+     * @param {string} kabupaten nama kabupaten, contoh kab. magelang 
+     * data harus sama persis dengan result pada Provinsi()
+     * @returns 
+     */
+    static Kabupaten = async (kabupaten) => {
+        const { data } = await axios.get(baseURL.dataWeb + '/API/cuaca/data-kecamatan.bmkg?kab=' + kabupaten)
+        let json = xml_parser.parse(data)
+        if (json.data == '') {
+            return []
+        } else {
+            return json.data.kec
+        }
+    }
+
+    static cuacaWeb = async (id) => {
+        const { data } = await axios.get(baseURL.dataWeb + '/API/cuaca/cuaca-kecamatan.bmkg?id=' + id + '&detail=1')
+        let json = xml_parser.parse(data)
+        return json.cuaca_kecamatan
+    }
+
+    static cuacaApp = async (id) => {
+        const { data } = await axios.get(baseURL.dataApp + '/api/cuaca/kec-id/lengkap/' + id)
+        return data
+    }
+}
+
+module.exports = BMKG;
